7ab84e1eb4b6bf71703b32a7bb2420c6cf9a3b70
Chase Derr <Chisnits@users.noreply.github.com>
2017-03-28 15:49:36 -0600

    complete

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import friends from "../components/friends";
+import Friend from '../components/Friend';
+
+class FriendsList extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            searchText: "",
+            orderBy: "name",
+            order: "ascending"
+        }
+    }
+
+    handleChange(field, event){
+        this.setState({
+        [field]: event.target.value
+        });
+    }
+
+     render() {
+         const friendsList = friends
+            .filter( friend => friend.name.toLowerCase().indexOf( this.state.searchText.toLowerCase() ) !== -1 )
+            .sort( ( a, b ) => a[ this.state.orderBy ] > b[ this.state.orderBy ] )
+            .map( friend => (
+                <Friend
+                    currentLocation={friend.current_location || {} }
+                    friendCount={ friend.friend_count }
+                    key={ friend.$$hashKey }
+                    name={ friend.name }
+                    pictureUrl={ friend.pic_square }
+                    status={ friend.status ? friend.status.message : "" }
+                />
+		) );
+         return(
+            <div>
+                <form className="form-inline searchForm" role="form">
+                    <div className="form-group">
+
+                        <input 
+                            className="form-control"
+                            onChange={this.handleChange.bind(this, "searchText")} 
+                            placeholder="Search For Friends"
+                            value={this.state.searchText}
+                        />
+
+                        <select 
+                            className="input-medium"
+                            onChange={this.handleChange.bind(this, "orderBy")} 
+                            value={this.state.orderBy}
+                            >
+                            <option>Name</option>
+                            <option>#Friends</option>
+                        </select>
+
+                        <select 
+                            className="input-medium"
+                            onChange={this.handleChange.bind(this, "order")} 
+                            value={this.state.order}
+                        >
+                            <option>Descending</option>
+                            <option>Ascending</option>
+                        </select>
+
+                    </div>
+                </form>
+
+                <ul>
+                    {friendsList}
+            </ul>
+        </div>
+        );
+    }
+}
+
+export default FriendsList;
